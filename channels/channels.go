25d381beb0cc3af019eb9dd8eb749130dc15bf7c
Mike Schinkel <mike@newclarity.net>
2023-10-21 00:40:19 -0400

    Add generic `channels` package

---
+++ channels/channels.go
@@ -0,0 +1,43 @@
+package channels
+
+import (
+	"context"
+)
+
+func WriteTo[T any](ctx context.Context, ch chan<- T, value T) (err error) {
+	for {
+		select {
+		case ch <- value:
+		case <-ctx.Done():
+			err = ctx.Err()
+			goto end
+		}
+	}
+end:
+	return err
+}
+
+func ReadFrom[T any](ctx context.Context, ch <-chan T, f func(value T) error) (err error) {
+	for {
+		select {
+		case <-ctx.Done():
+			err = ctx.Err()
+			goto end
+		case value, ok := <-ch:
+			if !ok {
+				// Channel is closed, we are done
+				goto end
+			}
+			err = f(value)
+			if err != nil {
+				goto end
+			}
+		}
+	}
+end:
+	return err
+}
+
+func Close[T any](ch chan T) {
+	close(ch)
+}
