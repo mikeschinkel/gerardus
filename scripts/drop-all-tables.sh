3c056825c89d516ad33f451b23ef6cdef1b8a8e4
Mike Schinkel <mike@newclarity.net>
2023-10-20 23:44:17 -0400

    Fix typo on emvironemnt variable name

--- scripts/drop-all-tables.sh
+++ scripts/drop-all-tables.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [ "${GERARDOUS_DB}" == "" ] ; then
-  echo "The env var GERARDOUS_DB not set"
+if [ "${GERARDUS_DB}" == "" ] ; then
+  echo "The env var GERARDUS_DB not set"
   exit 1
 fi
 
-if [ ! -f "${GERARDOUS_DB}" ] ; then
-  echo "The filepath '${GERARDOUS_DB}' DOES NOT EXIST; filepath from env var GERARDOUS_DB."
+if [ ! -f "${GERARDUS_DB}" ] ; then
+  echo "The filepath '${GERARDUS_DB}' DOES NOT EXIST; filepath from env var GERARDUS_DB."
   exit 2
 fi
 
 # Get the list of all tables in the SQLite database
-tables=$(sqlite3 "${GERARDOUS_DB}" \
+tables=$(sqlite3 "${GERARDUS_DB}" \
   "SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE '%sqlite_%';")
 
 if [ "${tables}" == "" ]; then
@@ -22,6 +22,6 @@ fi
 # Loop through each table name and drop it
 for table in $tables; do
   echo "Dropping ${table}..."
-  sqlite3 "${GERARDOUS_DB}" "DROP TABLE ${table};"
+  sqlite3 "${GERARDUS_DB}" "DROP TABLE ${table};"
 done
 echo "Done."
