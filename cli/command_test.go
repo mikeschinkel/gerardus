67765579bc3c64d9ac74ca9d37791decc4a2eee4
Mike Schinkel <mike@newclarity.net>
2023-12-14 20:41:13 -0500

    Fix `command_test.go` for recent changes

--- cli/command_test.go
+++ cli/command_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestSetExecFunc(t *testing.T) {
-	ef := func(cli.ArgsMap) error { return nil }
+	ef := func(*cli.CommandInvoker) error { return nil }
 	tests := []struct {
 		name string
 		ef   cli.ExecFunc
