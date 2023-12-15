d0c49fc15eafe805cc3a12b749d62b65c3693ed6
Mike Schinkel <mike@newclarity.net>
2023-12-15 01:49:45 -0500

    Add DI for testing

--- app/app_test.go
+++ app/app_test.go
@@ -8,15 +8,18 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/mikeschinkel/gerardus/app"
 	"github.com/mikeschinkel/gerardus/cli"
+	"github.com/mikeschinkel/gerardus/persister"
 	. "github.com/mikeschinkel/go-lib"
+	"github.com/mikeschinkel/go-serr"
 )
 
 func TestAppMain(t *testing.T) {
 	tests := []struct {
-		name   string
-		args   []string
-		errStr string
-		stdErr string
+		name    string
+		args    []string
+		errStr  string
+		stdErr  string
+		ctxFunc func() context.Context
 	}{
 		{
 			name:   "No CLI arguments",
@@ -42,6 +45,35 @@ func TestAppMain(t *testing.T) {
 			stdErr: addProjectGoLangOutput(),
 			errStr: "argument cannot be empty [arg_name='<repo_url>']",
 		},
+		{
+			name:   "add project golang https://not.there",
+			args:   []string{"add", "project", "golang", "https://not.there"},
+			stdErr: addProjectGoLangNotThereOutput(),
+			errStr: "not a valid GitHub repo URL [repo_url='https://not.there']",
+		},
+		{
+			name:   "add project golang https://github.com/not/there",
+			args:   []string{"add", "project", "golang", "https://github.com/not/there"},
+			stdErr: addProjectGoLangGitHubNotThereOutput(),
+			errStr: "",
+			ctxFunc: func() context.Context {
+				return context.WithValue(context.Background(), app.DIKey, &app.DI{
+					CheckURLFunc: CheckURLMock,
+				})
+			},
+		},
+		//{
+		//	name:   "add project golang http://github.com/golang/go",
+		//	args:   []string{"add", "project", "golang", "http://github.com/golang/go"},
+		//	stdErr: addProjectGoLangGitHubGolangGo(),
+		//	errStr: "",
+		//	ctxFunc: func() context.Context {
+		//		return context.WithValue(context.Background(), app.DIKey, &app.DI{
+		//			RepoInfoRequesterFunc: RepoInfoRequesterMock,
+		//			UpsertProjectFunc:     UpsertProjectMock,
+		//		})
+		//	},
+		//},
 		//{
 		//	name:    "add",
 		//	args:    []string{"codebase", "golang", "1.21.4"},
@@ -56,7 +88,11 @@ func TestAppMain(t *testing.T) {
 	for _, tt := range tests {
 		tt.args = RightShift(tt.args, cli.ExecutableFilepath(app.AppName))
 		t.Run(tt.name, func(t *testing.T) {
-			help, err := app.Main(context.Background(), tt.args, app.MainOpts{})
+			ctx := context.Background()
+			if tt.ctxFunc != nil {
+				ctx = tt.ctxFunc()
+			}
+			help, err := app.Main(ctx, tt.args, app.MainOpts{})
 			buf := bytes.Buffer{}
 			help.Usage(err, &buf)
 			if tt.stdErr != buf.String() {
@@ -74,6 +110,28 @@ func TestAppMain(t *testing.T) {
 			}
 		})
 	}
+}
+func CheckURLMock(url string) (err error) {
+	switch url {
+	case "https://github.com/not/there":
+		err = serr.New("oops")
+	}
+	return err
+}
+func RepoInfoRequesterMock(url string) (ri persister.RepoInfo, err error) {
+	switch url {
+	case "https://github.com/not/there":
+		err = serr.New("oops")
+	case "https://github.com/golang/go":
+		ri = persister.RepoInfo{
+			Description: "The Go programming language",
+			Homepage:    "https://go.dev",
+		}
+	}
+	return ri, err
+}
+func UpsertProjectMock(ctx context.Context, params persister.UpsertProjectParams) (p persister.Project, err error) {
+	return p, err
 }
 
 func noCLIArgsOutput() string {
@@ -115,29 +173,31 @@ ERROR: There is no 'add' command, but there are these commands:
 func addProjectOutput() string {
 	return `
 ERROR: Argument cannot be empty [arg_name='<project>']:
-
-  Usage: gerardus [<options>] <command> [<args>]
-
-  Command:
-
-    - add project <project> <repo_url> [<about> [<website>]]
-
-        Args:
-
-          project:   Project name, e.g. 'golang'
-          repo_url:  The full GitHub repository URL for the project, e.g. https://github.com/golang/go
-          about:     Repo description. Defaults to 'about' from the GitHub API
-          website:   Project website URL. Defaults to 'website' from the GitHub API
-
-        Global Options:
-
-          -data=<data_file>: Data file (sqlite3)
-`
+` + projectUsage()
 }
 func addProjectGoLangOutput() string {
 	return `
 ERROR: Argument cannot be empty [arg_name='<repo_url>']:
+` + projectUsage()
+}
 
+func addProjectGoLangNotThereOutput() string {
+	return `
+ERROR: Not a valid GitHub repo URL [repo_url='https://not.there']:
+` + projectUsage()
+}
+func addProjectGoLangGitHubNotThereOutput() string {
+	return `
+ERROR: URL could not be dereferenced [repo_url='https://github.com/not/there']:
+` + projectUsage()
+}
+
+func addProjectGoLangGitHubGolangGo() string {
+	return ``
+}
+
+func projectUsage() string {
+	return `
   Usage: gerardus [<options>] <command> [<args>]
 
   Command:
