89668f663c07e3561a3db6b40241452aaf04dd20
Mike Schinkel <mike@newclarity.net>
2023-12-20 05:41:22 -0500

    Add `moq` to generate `./app/query.stub.go`

--- persister/ddl.go
+++ persister/ddl.go
@@ -6,6 +6,7 @@ import (
 
 //go:generate sqlc generate -f ./sqlc.yaml
 //go:generate ifacemaker -f *.go -s Queries -i DataStoreQueries -p persister -o query.iface.go
+//go:generate moq -rm -skip-ensure -with-resets -pkg app -out ../app/query.stub.go . DataStoreQueries:DataStoreQueriesStub
 
 //go:embed schema.sql
 var ddl string
