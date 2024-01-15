07cc797960f70c80e4acd44a9d35532beaddfff2
Mike Schinkel <mike@newclarity.net>
2024-01-14 20:00:09 -0500

    Switch to custom sqlc `v1.25.0`

--- persister/ddl.go
+++ persister/ddl.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 )
 
+// IMPORTANT: Required version of sqlc incorporates this PR: https://github.com/sqlc-dev/sqlc/pull/3135
 //go:generate sqlc generate -f ./sqlc.yaml
 //go:generate ifacemaker -f *.go -s Queries -i DataStoreQueries -p persister -o query.iface.go
 //go:generate moq -rm -skip-ensure -with-resets -pkg app -out ../app/query.stub.go . DataStoreQueries:DataStoreQueriesStub
